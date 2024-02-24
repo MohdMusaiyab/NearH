d3e6182f0e7c39753c0f4dc6cc86bb89362e5566
Neba Fatima <thenebafatima@gmail.com>
2024-02-24 16:26:40 +0530

    about section added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
-    </div>
+    <>
+    <h1 className="text-2xl font-bold mb-4 ml-4 mt-8">About NearH</h1>
+    <p className='text-1xl mb-4 ml-4'>At NearH, we understand the importance of accessible and affordable healthcare services.<br/>
+    Whether you're looking for the nearest hospital with the most affordable services or comparing options for a specific medical procedure, NearH is here to help you make informed choices for your health and well-being.
+    Our platform is designed to empower individuals and families by providing comprehensive comparisons of hospital services based on availability and cost.</p>
+    <h2 className='text-xl font-bold mb-2 ml-4 mt-8 '>Our Mission</h2>
+    <p className='text-1xl mb-4 ml-4'>
+    <b>Our mission is simple:</b> <i>To make healthcare more transparent and accessible for everyone.</i>
+    <br/>Therefore, helping poeple find the right healthcare provider for their needs is what we stand for.We believe that by providing clear and concise information about hospital services, patients can make informed decisions about their healthcare needs. The financial aspect of healthcare is crucial and NearH enables users to compare the costs of various services across different hospitals, ensuring transparency and affordability.
+    </p>
+    <h2 className='text-xl font-bold mb-2 ml-4 mu-4'>How NearH Works</h2>
+    <p className="text-1xl mb-4 ml-4">NearH aggregates data from registered hospitals, including information on the availability of services and associated costs.<br/>
+     Our platform provides users with a user-friendly interface to easily compare different hospitals and their offerings.
+     <br/>We encourage hospitals and medical centers to get registered with us and be a part of our growing network. By registering with NearH, healthcare providers can reach a wider audience and showcase their services to individuals seeking quality and affordable healthcare options.
+    </p>
+     <h2 className='text-xl font-bold mb-2 ml-4'>Get Started with NearH</h2>
+     <p className='text-1xl mb-4 ml-4'>Join NearH today and take control of your healthcare decisions.<br/>
+     
+
+</p>
+
+
+    
+    </>
   )
 }
 
