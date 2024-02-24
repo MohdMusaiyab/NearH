56f014a3097270e6d8509477ca79dad1e418e3b0
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2024-02-24 12:20:51 +0530

    Routing Added in Front End

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import { Link } from 'react-router-dom';
 function App() {
   
   return (
     <>
       <h1 className="text-red-800 text-3xl">Hello..</h1>
+      <h1>Home Page</h1>
+      <Link to="/sign-in">Sign In</Link>
     </>
   )
 }
---
+++ client/src/pages/CreateHospital.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateHospital = () => {
+  return (
+    <div>
+      <h1>create hospital</h1>
+    </div>
+  )
+}
+
+export default CreateHospital
